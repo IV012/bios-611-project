a785557d21130eaa615301c591ceba0ec5977a52
Yifan <yifandai@yeah.net>
2022-12-06 04:58:02 -0500

    change the model structure, make it more robust

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader
 
 class BiLSTM_SentimentAnalysis(torch.nn.Module) :
-    def __init__(self, vocab_size, embedding_dim=128, hidden_dim=32, dropout=0.5, batch_size=64) :
+    def __init__(self, vocab_size, embedding_dim=64, hidden_dim=32, dropout=0.5, batch_size=64) :
         super().__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=2, batch_first=True, bidirectional=True)
@@ -13,6 +13,7 @@ class BiLSTM_SentimentAnalysis(torch.nn.Module) :
             nn.Dropout(dropout),
             nn.ReLU()
         )
+        self.dropout = nn.Dropout(dropout)
         self.dense = nn.Sequential(
             nn.Linear(2*hidden_dim, 1),
             nn.Sigmoid()
@@ -25,7 +26,8 @@ class BiLSTM_SentimentAnalysis(torch.nn.Module) :
         """
         embs = self.embedding(x)
         out, hidden = self.lstm(embs, hidden)
-        out = self.fc(out)
+        out = self.dropout(out)
+        # out = self.fc(out)
         out = self.dense(out)
         out = out.squeeze()
         out = out[:, -1]
